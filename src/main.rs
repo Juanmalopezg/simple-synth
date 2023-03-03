87134a52a2d24dfca86d87c5d3c1e7f084e3cacb
Juanma Lopez G <juanmaa79@gmail.com>
2023-03-02 22:19:42 -0300

    Add structure refactor and comments

--- src/main.rs
+++ src/main.rs
@@ -2,98 +2,92 @@ use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use std::f32::consts::PI;
 use std::time::Duration;
 
-fn main() {
-    let host = cpal::default_host();
-    let device = host.default_output_device().expect("Failed to get default output device");
-    let config = device.default_output_config().expect("Failed to get default output config");
-    let sample_rate = config.sample_rate().0 as f32;
-
-    let freq1: f32 = 440.0;
-    let freq2: f32 = 550.0;
-    let freq3: f32 = 660.0;
-    let amp1: f32 = 0.1;
-    let amp2: f32 = 0.1;
-    let amp3: f32 = 0.1;
-    let waveform1: Waveform = Waveform::Sine;
-    let waveform2: Waveform = Waveform::Square;
-    let waveform3: Waveform = Waveform::Sawtooth;
-    let speed1: f32 = 1.0;
-    let speed2: f32 = 1.0;
-    let speed3: f32 = 1.0;
-
-
-    let mut oscillators = vec![
-        Oscillator::new(freq1, amp1, waveform1, speed1),
-        Oscillator::new(freq2, amp2, waveform2, speed2),
-        Oscillator::new(freq3, amp3, waveform3, speed3),
-    ];
-
-    let stream = device.build_output_stream(
-        &config.into(),
-        move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-            for (_i, sample) in data.iter_mut().enumerate() {
-                let mut sample_sum = 0.0;
-                for oscillator in &mut oscillators {
-                    let oscillator_sample = oscillator.next_sample(sample_rate);
-                    sample_sum += oscillator_sample;
-                }
-                *sample = sample_sum;
-            }
-        }
-        ,
-        |err| eprintln!("An error occurred on the audio stream: {}", err),
-        None,
-    )
-        .unwrap();
-
-    stream.play().unwrap();
-
-    std::thread::sleep(Duration::from_secs(5));
+// Definimos una estructura para los parámetros de un oscilador
+struct OscillatorParams {
+    frequency: f32,
+    amplitude: f32,
+    waveform: Waveform,
+    speed: f32,
+    phase: f32,
 }
 
+// Definimos una estructura para los osciladores
+struct Oscillator {
+    params: OscillatorParams,
+}
+
+// Definimos una enumeración para los tipos de onda
 enum Waveform {
     Sine,
     Square,
     Sawtooth,
 }
 
-struct Oscillator {
-    frequency: f32,
-    amplitude: f32,
-    waveform: Waveform,
-    phase: f32,
-    speed: f32,
-}
-
-
 impl Oscillator {
-    fn new(frequency: f32, amplitude: f32, waveform: Waveform, speed: f32) -> Self {
+    fn new(params: OscillatorParams) -> Self {
         Self {
-            frequency,
-            amplitude,
-            waveform,
-            phase: 0.0,
-            speed,
+            params,
         }
     }
 
     fn next_sample(&mut self, sample_rate: f32) -> f32 {
-        let period = 1.0 / (self.frequency * self.speed);
+        let period = 1.0 / (self.params.frequency * self.params.speed);
         let phase_increment = 1.0 / sample_rate * (2.0 * PI / period);
-        self.phase = (self.phase + phase_increment) % 1.0;
-        let sample = match self.waveform {
-            Waveform::Sine => self.amplitude * ((2.0 * PI * self.phase)).sin(),
+        self.params.phase = (self.params.phase + phase_increment) % 1.0;
+        let phase = self.params.phase;
+        let sample = match self.params.waveform {
+            Waveform::Sine => self.params.amplitude * ((2.0 * PI * phase)).sin(),
             Waveform::Square => {
-                if (2.0 * PI * self.phase).sin() > 0.0 {
-                    self.amplitude
+                if (2.0 * PI * phase).sin() > 0.0 {
+                    self.params.amplitude
                 } else {
-                    -self.amplitude
+                    -self.params.amplitude
                 }
             }
             Waveform::Sawtooth => {
-                (2.0 * self.amplitude / PI) * (self.frequency * PI * self.phase).sin().asin()
+                (2.0 * self.params.amplitude / PI) * (self.params.frequency * PI * phase).sin().asin()
             }
         };
         sample
     }
+}
+
+fn main() {
+    // Configuramos el dispositivo de salida
+    let host = cpal::default_host();
+    let device = host.default_output_device().expect("Failed to get default output device");
+    let config = device.default_output_config().expect("Failed to get default output config");
+    let sample_rate = config.sample_rate().0 as f32;
+
+    // Creamos los osciladores
+    let oscillator1 = create_oscillator(440.0, 0.1, Waveform::Sine, 1.0, 0.0);
+    let oscillator2 = create_oscillator(550.0, 0.1, Waveform::Square, 1.0, 0.0);
+    let oscillator3 = create_oscillator(660.0, 0.1, Waveform::Sawtooth, 1.0, 0.0);
+
+    let mut oscillators = vec![oscillator1, oscillator2, oscillator3];
+
+    // Creamos el stream de audio
+    let stream = device.build_output_stream(&config.into(), move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+        for (_i, sample) in data.iter_mut().enumerate() {
+            let mut sample_sum = 0.0;
+            for oscillator in &mut oscillators {
+                let oscillator_sample = oscillator.next_sample(sample_rate);
+                sample_sum += oscillator_sample;
+            }
+            *sample = sample_sum;
+        }
+    }, |err| eprintln!("An error occurred on the audio stream: {}", err), None, ).unwrap();
+    stream.play().unwrap();
+    std::thread::sleep(Duration::from_secs(50));
+}
+
+fn create_oscillator(frequency: f32, amplitude: f32, waveform: Waveform, speed: f32, phase: f32) -> Oscillator {
+    let params = OscillatorParams {
+        frequency,
+        amplitude,
+        waveform,
+        speed,
+        phase,
+    };
+    Oscillator::new(params)
 }
